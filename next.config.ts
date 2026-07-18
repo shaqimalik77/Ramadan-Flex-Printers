b604d7633b862a39e19927bfc948847533f2b96a
fatima5958 <mateenfatima5958@gmail.com>
2026-07-18 05:41:00 +0500

    Update package file

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        // Apply no‑cache headers to all HTML pages and JSON data
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, max-age=0, must-revalidate',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 
